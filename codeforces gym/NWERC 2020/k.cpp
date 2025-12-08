115241f15e9d59d7529407b2db150bbf900c7d85
Gabriel Abreu <g.abreu@aluno.ufabc.edu.br>
2025-12-08 02:10:23 -0300

    create k.cpp

---
+++ codeforces gym/NWERC 2020/k.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+
+    string s, t;
+    getline(cin, s);
+    getline(cin, t);
+
+
+    map<char, int> a, b;
+
+    for(auto &x : s) a[x]++;
+    for(auto &x : t) b[x]++;
+
+    if(a[' '] != b[' ']) cout << ' ';
+
+    for(auto &[x, y] : a){
+        if(x == ' ') continue;
+        if(y != b[x]){
+            cout << x;
+        }
+    }
+
+}
